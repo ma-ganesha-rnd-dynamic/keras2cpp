8213b15a1b4b6c60c474547cdc9c7cd4b2bc517a
abid123456 <abdilh_ahmad_official@yahoo.com>
2024-12-20 14:50:52 +0700

    adjust mnist python script

--- example/mnist_cnn_one_iteration.py
+++ example/mnist_cnn_one_iteration.py
@@ -10,9 +10,8 @@ np.random.seed(1337)  # for reproducibility
 
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.utils import np_utils
+from keras.layers import Dense, Dropout, Activation, Flatten, Convolution2D, MaxPooling2D
+from keras.utils import to_categorical
 
 batch_size = 128
 nb_classes = 10
@@ -30,8 +29,8 @@ nb_conv = 3
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 X_train /= 255
@@ -41,15 +40,15 @@ print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+Y_train = to_categorical(y_train, nb_classes)
+Y_test = to_categorical(y_test, nb_classes)
 
 model = Sequential()
 
-model.add(Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='same',
-                        input_shape=(1, img_rows, img_cols)))
+model.add(Convolution2D(nb_filters, 3, 3,
+                        input_shape=(img_rows, img_cols, 1)))
 model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='same'))
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
 model.add(Dropout(0.25))
@@ -63,14 +62,15 @@ model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
               optimizer='adadelta')
+print(model.summary())
 
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+model.fit(X_train, Y_train, batch_size=batch_size,
           verbose=1, validation_data=(X_test, Y_test))
 
 # store model
 with open('./my_nn_arch.json', 'w') as fout:
     fout.write(model.to_json())
-model.save_weights('./my_nn_weights.h5', overwrite=True)
+model.save_weights('./my_nn.weights.h5', overwrite=True)
 
 # store one sample in text file
 with open("./sample_mnist.dat", "w") as fin:
